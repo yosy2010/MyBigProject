150e207aa9b529836b89da874cde35178f13aa56
Yasser Altamimi <yosy2010@gmail.com>
2018-09-25 00:05:36 +0300

    Initial Commit

---
+++ MyBigProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyBigProject
+//
+//  Created by YASSER ALTAMIMI on 25/09/2018.
+//  Copyright © 2018 YASSER ALTAMIMI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyBigProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyBigProject
+//
+//  Created by YASSER ALTAMIMI on 25/09/2018.
+//  Copyright © 2018 YASSER ALTAMIMI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
